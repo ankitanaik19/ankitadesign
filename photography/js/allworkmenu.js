051da3d73c487eeff6feff5a64fb97e437316058
ankitanaik19 <68180489+ankitanaik19@users.noreply.github.com>
2020-09-10 10:16:56 +1200

    3update

--- photography/js/allworkmenu.js
+++ photography/js/allworkmenu.js
@@ -4,13 +4,5 @@ $(document).ready(function() {
        $("#allwork").addClass("animated fadeInLeft");
        $("#workrow1").addClass("animated flipInX");
        
-        $("#clients").show();
-        $("#quote").show();
-        $("#aboutcontent").hide();
-        $("#contact").hide();
-        $("#gallery").hide();
-        $("#services").hide();
-        $("#features").hide();
-        $(".footer").show();
     });
 });
