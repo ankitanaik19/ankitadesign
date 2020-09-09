89823eb098f516799a4aef519b9622e9ff19729d
ankitanaik19 <68180489+ankitanaik19@users.noreply.github.com>
2020-09-09 16:41:40 +1200

    css

--- photography/js/serviceleftload.js
+++
@@ -1,8 +0,0 @@
-$(window).on("scroll", function() {
-    if ($(window).scrollTop()) {
-        $("#left").addClass("servicelefttload");
-
-    } else {
-        $("#left").removeClass("servicelefttload");
-    }
-})
